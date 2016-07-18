07ed5d54dc6557679c7b73e0c2c4bcf1a70035e5
pdurangop <pdurangop@gmail.com>
2016-07-18 11:55:55 -0500

    proyecto creacion manual

---
+++ loquesea/app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id]) #busca por el id
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_url
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html {redirect_to users_url, notice: "Se ha guardado el Usuario"}
+
+        format.json {render json: users_url, status: :create, location: users_url}
+      else
+        format.html {render action: "new"}
+      end
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :id, :apellido)
+  end
+end
